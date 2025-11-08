dc1109bfaf21e74ae46e2ffc0c17b6a94375ce12
delberss <delberss@hotmail.com>
2025-11-08 20:41:45 -0300

    feat: adiciona rota de registro e inicia página

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ConfirmacaoPage from './pages/ConfirmacaoPage.jsx'
 import LoginPage from './pages/LoginPage.tsx'
+import RegistroPage from './pages/RegistroPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/registro" element={<RegistroPage />} />
       <Route path="/confirmacao" element={<ConfirmacaoPage/>} />
     </Routes>
   </BrowserRouter>
---
+++ src/pages/RegistroPage.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export default function RegistroPage() {
+  return (
+    <Box textAlign="center" mt={6} px={2}>
+      Página de registro
+    </Box>
+  );
+}
