1cdd5ba322488d4b84ede3418264943359f36197
delberss <delberss@hotmail.com>
2025-11-08 19:47:51 -0300

    feat(paginas): construindo página de Confirmação de ingresso

---
+++ src/pages/ConfirmacaoPage.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+
+export default function ConfirmacaoPage() {
+  
+  return (
+    <Box textAlign="center" mt={2} mb={6}>
+      Página de Confirmação de ingresso
+    </Box>
+  );
+}
