8f722a0ec4e9e888315e5b2f2fa66982e90d0f38
delberss <delberss@hotmail.com>
2025-11-08 19:48:40 -0300

    feat(rotas): adicionar rota /confirmacao para ConfirmacaoPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ConfirmacaoPage from './pages/ConfirmacaoPage.jsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/confirmacao" element={<ConfirmacaoPage/>} />
     </Routes>
   </BrowserRouter>
 )
