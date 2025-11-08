6e3a8025adb2dd895a0b4319d1052f6c9fdaf842
delberss <delberss@hotmail.com>
2025-11-08 02:00:17 -0300

    feat(store): adiciona usuário, login e gerenciamento de ingressos

--- src/store/useCinemaStore.ts
+++ src/store/useCinemaStore.ts
@@ -4,12 +4,39 @@ import { persist } from "zustand/middleware";
 interface HorarioSelecionado {
   filme: string;
   hora: string;
-  data: string
+  data: string;
   quantidade: number;
 }
+
+interface Usuario {
+  cpf: string;
+  email: string;
+}
+
+interface Ingresso {
+  id: string;
+  filme: string;
+  hora: string;
+  data: string;
+  dataCompra: string;
+  valor: number;
+  quantidade: number;
+}
+
+
 interface CinemaState {
   horarioSelecionado: HorarioSelecionado | null;
   setHorarioSelecionado: (data: HorarioSelecionado | null) => void;
+
+  usuario: Usuario | null;
+  setUsuario: (data: Usuario | null) => void;
+
+  isLoggedIn: boolean;
+  setIsLoggedIn: (value: boolean) => void;
+
+  ingressosComprados: Ingresso[];
+  adicionarIngresso: (ingresso: Ingresso) => void;
+  removerIngresso: (id: string) => void; // 👈 nova função
 }
 
 export const useCinemaStore = create<CinemaState>()(
@@ -17,11 +44,33 @@ export const useCinemaStore = create<CinemaState>()(
     (set) => ({
       horarioSelecionado: null,
       setHorarioSelecionado: (data) => set({ horarioSelecionado: data }),
+
+      usuario: null,
+      setUsuario: (data) => set({ usuario: data }),
+
+      isLoggedIn: false,
+      setIsLoggedIn: (value) => set({ isLoggedIn: value }),
+
+      ingressosComprados: [],
+      adicionarIngresso: (ingresso) =>
+        set((state) => ({
+          ingressosComprados: [...state.ingressosComprados, ingresso],
+        })),
+
+      removerIngresso: (id) =>
+        set((state) => ({
+          ingressosComprados: state.ingressosComprados.filter(
+            (i) => i.id !== id
+          ),
+        })),
     }),
     {
       name: "cinema-storage",
       partialize: (state) => ({
         horarioSelecionado: state.horarioSelecionado,
+        usuario: state.usuario,
+        isLoggedIn: state.isLoggedIn,
+        ingressosComprados: state.ingressosComprados,
       }),
     }
   )
