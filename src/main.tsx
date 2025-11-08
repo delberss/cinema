9f911edb7cbd5e893839a7305cd75f6c6a73efbe
delberss <delberss@hotmail.com>
2025-11-08 20:35:22 -0300

    feat: adiciona rota de login e inicia página

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ConfirmacaoPage from './pages/ConfirmacaoPage.jsx'
+import LoginPage from './pages/LoginPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/confirmacao" element={<ConfirmacaoPage/>} />
     </Routes>
   </BrowserRouter>
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+
+export default function LoginPage() {
+
+  return (
+    <Box textAlign="center" mt={6} px={2}>
+      Página para Login
+    </Box>
+  );
+}
